4256d00d5a63a750c002f557611944e48a7a6cc5
Min Than Htut <fellinglove8@gmail.com>
2020-07-27 18:00:25 +0630

    m3u8 video get

--- lib/src/video.dart
+++ lib/src/video.dart
@@ -1,5 +1,10 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:video_player/video_player.dart';
+import 'package:http/http.dart' as http;
+import 'model/m3u8.dart';
+import 'model/m3u8s.dart';
 
 class YoYoPlayer extends StatefulWidget {
   /// yoyo_player is a video player that allows you to select HLS video streaming by selecting the quality
@@ -13,20 +18,41 @@ class YoYoPlayer extends StatefulWidget {
 
 class _YoYoPlayerState extends State<YoYoPlayer> {
   VideoPlayerController controller;
+  List<M3U8> m3u8List = List();
+  String m3u8Content;
 
   @override
   void initState() {
     super.initState();
     initPlayer();
+    m3u8video();
   }
 
   void initPlayer() {}
 
-  void m3u8video(){
-
+  Future<M3U8s> m3u8video() async {
+    m3u8List.add(M3U8(quality: "Auto", url: widget.url));
+    RegExp regExp = new RegExp(
+      r"#EXT-X-STREAM-INF:(?:.*,RESOLUTION=(\d+x\d+))?,?(.*)\r?\n(.*)",
+      caseSensitive: false,
+      multiLine: true,
+    );
+    if (m3u8Content == null && widget.url != null) {
+      http.Response response = await http.get(widget.url);
+      if (response.statusCode == 200) {
+        m3u8Content = utf8.decode(response.bodyBytes);
+      }
+    }
+    List<RegExpMatch> matches = regExp.allMatches(m3u8Content).toList();
+    matches.forEach((RegExpMatch regExpMatch) {
+      String quality = (regExpMatch.group(1)).toString();
+      String url = (regExpMatch.group(3)).toString();
+      print(url);
+      m3u8List.add(M3U8(quality: quality, url: url));
+    });
+    M3U8s m3u8s = M3U8s(m3u8s: m3u8List);
+    return m3u8s;
   }
-
-  
 
   @override
   Widget build(BuildContext context) {
