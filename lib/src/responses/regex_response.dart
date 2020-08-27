de81fc82388654bc2cb42c5d5f3af058857c2488
Ko Htut <fellinglove8@gmail.com>
2020-08-27 14:58:17 +0630

    add : regex

---
+++ lib/src/responses/regex_response.dart
@@ -0,0 +1,16 @@
+class Rexexresponse {
+  static String regexMAIN =
+      "#EXT-X-STREAM-INF:(?:.*,RESOLUTION=(\d+x\d+))?:.*,\r?\n(.*)";
+  static String regexMEDIA =
+      r"""^#EXT-X-MEDIA:TYPE=AUDIO,GROUP-ID="(.*)",NAME="(.*)",AUTOSELECT=(.*),DEFAULT=(.*),CHANNELS="(.*)",URI="(.*)""";
+  static String regexAUDIO = "";
+  static String regexSUBTITLE = "";
+  static String regexSRT =
+      r"^((\d{2}):(\d{2}):(\d{2}),(\d{3})) +--> +((\d{2}):(\d{2}):(\d{2}),(\d{2})).*[\r\n]+\s*((?:(?!\r?\n\r?).)*)";
+  static String regexASS = "";
+  static String regexVTT = "";
+  static String regexSTREAM = "";
+  static String regexFILE = "";
+  static String regexHTTP = r'^(http|https):\/\/([\w.]+\/?)\S*';
+  static String regexURL = r'(.*)\r?\/';
+}
