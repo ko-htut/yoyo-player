923e30894572848ad89fff0115c4f4a9f4607d36
MR <mohamedramadan949@gmail.com>
2023-08-17 11:11:15 -0700

    k

--- example/lib/YoYoPlayerScreen.dart
+++ example/lib/YoYoPlayerScreen.dart
@@ -32,9 +32,10 @@ class _YoYoPlayerScreenState extends State<YoYoPlayerScreen> {
               child: YoYoPlayer(
                 aspectRatio: 16 / 9,
                 url:
-                    // "https://vz-7b6a17bc-fc1.b-cdn.net/43babd4b-eaed-4a93-baa9-2c2ebf091fb6/playlist.m3u8?v=1689179659",
-                    // "https://flutter.github.io/assets-for-api-docs/assets/videos/bee.mp4",
-                    "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8",
+                    "https://vz-fd93b4ad-1aa.b-cdn.net/64a045f8-8935-4369-b83f-06f043ddf6b1/playlist.m3u8",
+                // "https://vz-7b6a17bc-fc1.b-cdn.net/43babd4b-eaed-4a93-baa9-2c2ebf091fb6/playlist.m3u8?v=1689179659",
+                // "https://flutter.github.io/assets-for-api-docs/assets/videos/bee.mp4",
+                // "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8",
                 // "https://player.vimeo.com/external/440218055.m3u8?s=7ec886b4db9c3a52e0e7f5f917ba7287685ef67f&oauth2_token_id=1360367101",
                 // "https://vz-d841190e-3d4.b-cdn.net/f0afa00b-459a-47ab-9d28-9952b8976e9d/playlist.m3u8?withoutwebview",
                 videoStyle: VideoStyle(
